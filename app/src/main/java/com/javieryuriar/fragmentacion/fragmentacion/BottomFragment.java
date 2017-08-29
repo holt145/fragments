6f99607970368ba9f720a6483c8aab05dd444385
holt145 <holt145gamer@hotmail.com>
2017-08-29 18:50:31 -0500

    first text

---
+++ app/src/main/java/com/javieryuriar/fragmentacion/fragmentacion/BottomFragment.java
@@ -0,0 +1,20 @@
+package com.javieryuriar.fragmentacion.fragmentacion;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alumno on 29/08/2017.
+ */
+
+public class BottomFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top,container,false);
+    }
+}
---
+++ app/src/main/java/com/javieryuriar/fragmentacion/fragmentacion/TopFragment.java
@@ -0,0 +1,10 @@
+package com.javieryuriar.fragmentacion.fragmentacion;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Alumno on 29/08/2017.
+ */
+
+public class TopFragment extends Fragment {
+}
